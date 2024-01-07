e9f6c984f42a4de7986ce2685d1c088b7c38b16f
jmvasanth <jmvasanth@protonmail.com>
2024-01-08 00:55:13 +1100

    Automate EC2 creation fix

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+#NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+NAMES=("mongodb")
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-0572c2ba2bf773656
@@ -22,14 +23,21 @@ do
 
     aws route53 change-resource-record-sets --hosted-zone-id Z0803879328RS2R0BHFXF --change-batch '
     {
-            "Changes": [{
-            "Action": "CREATE",
-                        "ResourceRecordSet": {
-                            "Name": “'$i.$DOMAIN_NAME'”,
-                            "Type": "A",
-                            "TTL": 300,
-                            "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
-                        }}]
+    "Changes":[
+        {
+            "Action":"CREATE",
+            "ResourceRecordSet":{
+                "Name":"'$i.$DOMAIN_NAME'",
+                "Type":"A",
+                "TTL":300,
+                "ResourceRecords":[
+                {
+                    "Value":"'$IP_ADDRESS'"
+                }
+                ]
+            }
+        }
+    ]
     }
     '
 done

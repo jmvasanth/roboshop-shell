0f09b9a87fa0dc9353da2d7e878196334f4b3aab
jmvasanth <jmvasanth@protonmail.com>
2024-01-08 00:23:23 +1100

    Automate EC2 creation

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+INSTANCE_TYPE=""
+IMAGE_ID=ami-03265a0778a880afb
+SECURITY_GROUP_ID=sg-0572c2ba2bf773656
+
+# If mysql and mongodb instance type is t3.medium, for all other it is t2.micro
 
 for i in "${NAMES[@]}"
 do 
-    echo "NAME: $i"
+    if [ [ $i == "mongodb" || $i == "mysql" ] ]
+    then
+        INSTANCE_TYPE="t3.medium"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+    echo "Creating $i instance"
+    aws ec2 run-instances --image-id $IMAGE_ID --instance-type t2.micro --security-group-ids $SECURITY_GROUP_ID  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
 done

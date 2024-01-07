478780bc1782bb705d6e6069c1fe9771a39cb469
jmvasanth <jmvasanth@protonmail.com>
2024-01-08 00:28:33 +1100

    Automate EC2 creation

--- create-ec2.sh
+++ create-ec2.sh
@@ -9,12 +9,13 @@ SECURITY_GROUP_ID=sg-0572c2ba2bf773656
 
 for i in "${NAMES[@]}"
 do 
-    if [ [ $i == "mongodb" || $i == "mysql" ] ]
+    if [[ $i == "mongodb" || $i == "mysql" ]]
     then
         INSTANCE_TYPE="t3.medium"
     else
         INSTANCE_TYPE="t2.micro"
     fi
     echo "Creating $i instance"
-    aws ec2 run-instances --image-id $IMAGE_ID --instance-type t2.micro --security-group-ids $SECURITY_GROUP_ID  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type t2.micro --security-group-ids $SECURITY_GROUP_ID  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIPAddress')
+    echo "Created $i instance: $IP_ADDRESS"
 done

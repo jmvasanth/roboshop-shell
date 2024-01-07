27529dc301d6b4d98c0f76fbf6235e636f288cb5
jmvasanth <jmvasanth@protonmail.com>
2024-01-08 10:20:17 +1100

    Automate EC2 creation fix

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+#NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+NAMES=$@
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-0572c2ba2bf773656
 DOMAIN_NAME=vgsk.online
+HOSTED_ZONE_ID=Z0803879328RS2R0BHFXF
 
 # If mysql and mongodb instance type is t3.medium, for all other it is t2.micro
 
-for i in "${NAMES[@]}"
+for i in $@
 do 
     if [[ $i == "mongodb" || $i == "mysql" ]]
     then
@@ -20,7 +22,7 @@ do
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type t2.micro --security-group-ids $SECURITY_GROUP_ID  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "Created $i instance: $IP_ADDRESS"
 
-    aws route53 change-resource-record-sets --hosted-zone-id Z0803879328RS2R0BHFXF --change-batch '
+    aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch '
     {
     "Changes":[
         {

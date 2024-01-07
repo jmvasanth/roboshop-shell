64677d7359b62329ea98a63c58451835f4c13532
jmvasanth <jmvasanth@protonmail.com>
2024-01-08 00:42:06 +1100

    Automate EC2 creation fix

--- create-ec2.sh
+++ create-ec2.sh
@@ -4,6 +4,7 @@ NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping"
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-0572c2ba2bf773656
+DOMAIN_NAME=vgsk.online
 
 # If mysql and mongodb instance type is t3.medium, for all other it is t2.micro
 
@@ -18,4 +19,17 @@ do
     echo "Creating $i instance"
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type t2.micro --security-group-ids $SECURITY_GROUP_ID  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "Created $i instance: $IP_ADDRESS"
+
+    aws route53 change-resource-record-sets --hosted-zone-id Z0803879328RS2R0BHFXF --change-batch ‘
+    {
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                            "Name": “$i.$DOMAIN_NAME”,
+                            "Type": "A",
+                            "TTL": 300,
+                            "ResourceRecords": [{ "Value": "$IP_ADDRESS"}]
+                        }}]
+    }
+    ‘
 done
